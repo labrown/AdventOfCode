ca86f3af22a6031ebe9e168f05ac5957a63c42d5
Lance A. Brown <lance@bearcircle.net>
2021-12-05 05:51:06 +0000

    Final solution for Day 04 - 2

--- Day04/day04-2.pl
+++ Day04/day04-2.pl
@@ -1,10 +1,11 @@
 #!/usr/bin env perl
 
+STDOUT->autoflush(1);
+
 use strict;
 use warnings;
 
 use File::Slurp;
-use Data::Dumper::Simple;
 
 # load data
 my @data = read_file('data.txt');
@@ -16,139 +17,117 @@ my @draws = split( /,/, shift @data );
 # discard next line
 shift @data;
 
-my @row_boards;
-my @col_boards;
-
-my $row_ref = [];
-my $col_ref = [ [], [], [], [], [] ];
+my @boards;
+my $board = [];
 
 for my $line (@data) {
-
-    # Process a board line
-    #print "line is >$line<\n";
-
     # Trim off leading space
     $line =~ s/^\s+//;
 
     if ( $line =~ /\d+\s+\d+\s+\d+\s+\d+\s+\d+$/ ) {
-        my @numbers = split( /\s+/, $line, 5 );
-        my @row     = map { { number => $_, played => 0 } } @numbers;
-        push @$row_ref, \@row;
-        for ( my $i = 0; $i < scalar(@row); $i++ ) {
-            my $aref = @$col_ref[$i];
-            push @$aref, { number => $row[$i]->{number}, played => 0 };
-        }
+        my $numbers = [ split( /\s+/, $line, 5 ) ];
+        push @$board, $numbers;
     }
 
     # Process an empty line.  Add the board to the boards array
     # and reset the row_ref
     elsif ( $line =~ /^$/ ) {
-        push @row_boards, $row_ref;
-        push @col_boards, $col_ref;
-        $row_ref = [];
-        $col_ref = [ [], [], [], [], [] ];
+        push @boards, $board;
+        $board = [];
     }
 }
-push @row_boards, $row_ref;
-push @col_boards, $col_ref;
+# Push last board into array
+push @boards, $board;
 
-# print Dumper(@row_boards);
-# print Dumper(@col_boards);
-
+# Print a board nicely
 sub print_board {
     my $board = shift;
     for my $row (@$board) {
         for my $spot (@$row) {
-            printf "%2d%s ", $$spot{number}, $$spot{played} ? 'X' : " ";
+            printf "%2s ", $spot;
         }
         print "\n";
     }
 }
 
-sub find_all_winner {
-    my $num_winners = 0;
-    for ( my $i = 0; $i < scalar(@row_boards); $i++ ) {
-        for my $board ( $row_boards[$i], $col_boards[$i] ) {
-            for my $row (@$board) {
-                my $bingo = scalar grep { $_->{played} == 1 } @$row;
-                if ( $bingo == 5 ) {
-                    $num_winners++;
-                }
-
+# Find the index of all the winners and return them for
+# processing
+sub find_all_winners {
+    my @win_indexes;
+    my $nb = scalar(@boards);
+    for ( my $i = 0; $i < $nb; $i++ ) {
+        my $board = $boards[$i];
+        my $bingo = 0;
+        # Check rows
+        for my $row (@$board) {
+            my $xes = scalar grep { "$_" eq "X" } @$row;
+            $bingo++ if $xes == 5;
+        }
+        # Check cols
+        my $nr = scalar @{ $$board[0] };
+        for ( my $j = 0; $j < $nr; $j++ ) {
+            my $xes = 0;
+            for ( my $ri = 0; $ri < $nr; $ri++ ) {
+                $xes++ if ( "$$board[$ri][$j]" eq "X" );
             }
+            $bingo++ if $xes == 5;
+        }
+        if ($bingo) {
+            push @win_indexes, $i;
         }
     }
-    print "num_winners is $num_winners\n";
+    return @win_indexes;
 }
 
-sub find_winner {
-    for ( my $i = 0; $i < scalar(@row_boards); $i++ ) {
-        for my $board ( $row_boards[$i], $col_boards[$i] ) {
-            for my $row (@$board) {
-                my $bingo = scalar grep { $_->{played} == 1 } @$row;
-                if ( $bingo == 5 ) {
-                    return ( $board, $i );
-                }
-
-            }
-        }
-    }
-    return ( undef, undef );
-}
-
-# print Dumper(@row_boards);
-# print Dumper(@col_boards);
-
-# Run the draws
+# Play teh game!
 for ( my $di = 0; $di < scalar(@draws); $di++ ) {
-
     my $draw = $draws[$di];
-    printf "Draw %3d is %d\n", $di, $draw;
-
-    printf "Playing %d boards\n", scalar(@row_boards);
+    printf "Draw #%d is %d\n", $di, $draw;
+    printf "Playing %d boards\n", scalar(@boards);
 
     # Play draw on all boards
-    for my $board ( @row_boards, @col_boards ) {
+    for my $board (@boards) {
         for my $row (@$board) {
+            my $ri = 0;
             for my $spot (@$row) {
-                if ( $$spot{number} == $draw ) {
-                    $$spot{played} = 1;
+                if ( "$spot" eq "$draw" ) {
+                    # Replace number with an 'X'
+                    splice( @$row, $ri, 1, "X" );
                 }
+                $ri++;
             }
         }
     }
 
     # Did we get a winner?
-    find_all_winner();
-    my ( $winner, $board_index ) = find_winner();
-    if ($winner) {
-        print "Winning draw is $draw\n";
-        print "Winning board index is $board_index\n";
+    my @winners = find_all_winners();
+    for my $wi (@winners) {
+        my $winner = $boards[$wi];
+        print "Winning board index is $wi\n";
         print "Wining board is:\n";
         print_board($winner);
 
+        # Sum up all unmatched numbers
         my $unmatched_sum = 0;
         for my $row (@$winner) {
             for my $spot (@$row) {
-                $unmatched_sum += $$spot{number} if $$spot{played} == 0;
+                $unmatched_sum += $spot if "$spot" ne "X";
             }
         }
 
-        printf "Unmatched numbers * winning draw is %d\n",
+        # Print final score of that board
+        printf "Sum of unmatched numbers * winning draw is %d\n\n",
             $unmatched_sum * $draw;
+    }
 
-        print "\n\n";
-
-        # Remove winning board from boards arrays
-        splice( @row_boards, $board_index, 1 );
-        splice( @col_boards, $board_index, 1 );
-
-        #print "after splice\n";
-        #print Dumper(@row_boards);
-        #print Dumper(@col_boards);
-
+    # Sort winner indexes into descending order to avoid
+    # splicing nonexistent index entries.
+    for my $wi ( sort { $b <=> $a } @winners ) {
+        # Remove winning board
+        splice( @boards, $wi, 1 );
     }
 
     # drop out if we run out of boards
-    last unless scalar @row_boards;
+    last unless scalar @boards > 0;
+    print "\n";
 }
