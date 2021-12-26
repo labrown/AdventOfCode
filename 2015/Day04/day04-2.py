449160b92fe6d70dac3a6b7460965663a724b3c9
Lance A. Brown <lance@bearcircle.net>
2021-12-26 17:21:08 +0000

    2015 Day 4

---
+++ 2015/Day04/day04-2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import hashlib
+
+filename = sys.argv[1]
+
+with open(filename) as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+key = lines[0]
+
+i = 1
+while 1:
+    if hashlib.md5((key + str(i)).encode('utf-8')).hexdigest().startswith('000000'):
+        print(i)
+        exit(0)
+    i += 1
